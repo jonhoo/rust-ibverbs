1d4c147775ccf6ed3e001b126d36fa964334340a
Neve Noomo <d3ad.l1n3@yandex.ru>
2020-02-22 17:36:52 +0300

    Add device querying

--- src/lib.rs
+++ src/lib.rs
@@ -257,6 +257,9 @@ impl<'devlist> Device<'devlist> {
     }
 }
 
+/// RDMA device attributes
+pub type DeviceAttr = ffi::ibv_device_attr;
+
 /// An RDMA context bound to a device.
 pub struct Context {
     ctx: *mut ffi::ibv_context,
@@ -376,6 +379,19 @@ impl Context {
         } else {
             Ok(ProtectionDomain { ctx: self, pd })
         }
+    }
+
+    /// Query corresponding device
+    pub fn query_device(&self) -> io::Result<DeviceAttr> {
+        let mut dev_attrs = Default::default();
+
+        let ret = unsafe { ffi::ibv_query_device(self.ctx, &mut dev_attrs) };
+
+        if ret != 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        Ok(dev_attrs)
     }
 }
 
@@ -543,7 +559,8 @@ impl<'res> QueuePairBuilder<'res> {
 
     /// Set the access flags of the new `QueuePair` such that it allows remote reads and writes.
     pub fn allow_remote_rw(&mut self) -> &mut Self {
-        self.access = self.access | ffi::ibv_access_flags::IBV_ACCESS_REMOTE_WRITE
+        self.access = self.access
+            | ffi::ibv_access_flags::IBV_ACCESS_REMOTE_WRITE
             | ffi::ibv_access_flags::IBV_ACCESS_REMOTE_READ;
         self
     }
@@ -828,7 +845,8 @@ impl<'res> PreparedQueuePair<'res> {
         attr.qp_access_flags = self.access.0;
         attr.pkey_index = 0;
         attr.port_num = PORT_NUM;
-        let mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE | ffi::ibv_qp_attr_mask::IBV_QP_PKEY_INDEX
+        let mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE
+            | ffi::ibv_qp_attr_mask::IBV_QP_PKEY_INDEX
             | ffi::ibv_qp_attr_mask::IBV_QP_PORT
             | ffi::ibv_qp_attr_mask::IBV_QP_ACCESS_FLAGS;
         let errno = unsafe { ffi::ibv_modify_qp(self.qp, &mut attr as *mut _, mask.0 as i32) };
@@ -851,7 +869,8 @@ impl<'res> PreparedQueuePair<'res> {
         attr.ah_attr.port_num = PORT_NUM;
         attr.ah_attr.grh.dgid = remote.gid;
         attr.ah_attr.grh.hop_limit = 0xff;
-        let mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE | ffi::ibv_qp_attr_mask::IBV_QP_AV
+        let mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE
+            | ffi::ibv_qp_attr_mask::IBV_QP_AV
             | ffi::ibv_qp_attr_mask::IBV_QP_PATH_MTU
             | ffi::ibv_qp_attr_mask::IBV_QP_DEST_QPN
             | ffi::ibv_qp_attr_mask::IBV_QP_RQ_PSN
@@ -870,7 +889,8 @@ impl<'res> PreparedQueuePair<'res> {
         attr.sq_psn = 0;
         attr.rnr_retry = self.rnr_retry;
         attr.max_rd_atomic = 1;
-        let mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE | ffi::ibv_qp_attr_mask::IBV_QP_TIMEOUT
+        let mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE
+            | ffi::ibv_qp_attr_mask::IBV_QP_TIMEOUT
             | ffi::ibv_qp_attr_mask::IBV_QP_RETRY_CNT
             | ffi::ibv_qp_attr_mask::IBV_QP_SQ_PSN
             | ffi::ibv_qp_attr_mask::IBV_QP_RNR_RETRY
