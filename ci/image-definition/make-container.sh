fb2855c5ff06ce8888f46b9245b386c3f8815211
Daniel Noland <daniel@bestateless.com>
2021-10-02 15:41:22 -0600

    Hacking on alpine build

--- ci/image-definition/make-container.sh
+++ ci/image-definition/make-container.sh
@@ -7,6 +7,7 @@ build_dir="$(readlink --canonicalize-existing "$(dirname "${0}")")"
 declare -r build_dir
 
 pushd "${build_dir}"
-docker buildx build --tag=rust_ibverbs_image "${build_dir}"
+docker buildx build --tag=rust_ibverbs_image_debian "${build_dir}"
+docker buildx build -f Dockerfile.alpine --tag=rust_ibverbs_image_alpine "${build_dir}"
 popd
 
