67598c77ee2736bc595c6d7054a79efb4268a8b2
Jon Gjengset <jon@thesquareplanet.com>
2021-11-06 12:40:30 -0700

    Prepare for release of -sys split

--- ibverbs-sys/src/lib.rs
+++ ibverbs-sys/src/lib.rs
@@ -3,6 +3,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(missing_docs)]
+// Suppress expected warnings from bindgen-generated code.
+// See https://github.com/rust-lang/rust-bindgen/issues/1651.
+#![allow(deref_nullptr)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 /// An ibverb work completion.
