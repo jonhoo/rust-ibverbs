ea1dec2ac6df710e52d24dfddc56d1af18e4d6bc
Jon Gjengset <jon@thesquareplanet.com>
2024-07-20 06:05:11 -0400

    Build outside of source dir (using cmake crate) (#36)

---
+++ configure-ci.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Install libibverbs dependencies"
+sudo apt-get update
+sudo apt-get install libibverbs1 libibverbs-dev
+
+echo "Install clang for rdma-core to be happy"
+sudo apt-get install clang
+
+echo "Move Cargo target dir to avoid super long paths"
+mkdir /tmp/cargo
+
+echo "Tell Cargo to use clang and target dir"
+mkdir .cargo
+cat >.cargo/config.toml <<EOF
+[build]
+target-dir = "/tmp/cargo"
+
+[env]
+CC = "clang"
+CXX = "clang++"
+LD = "clang"
+EOF
