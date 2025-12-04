51891a2070b6fd30429ae7b7ab75b5ff3313fa22
Ashwin Rajnish <ashwinrajnish@twitter.com>
2025-12-04 14:12:01 -0800

    fix

--- ibverbs/src/lib.rs
+++ ibverbs/src/lib.rs
@@ -1570,21 +1570,6 @@ impl<T> MemoryRegion<T> {
             lkey: unsafe { *self.inner.mr }.lkey,
         }
     }
-
-    /// Make a subslice of this memory region.
-    pub fn slice(&self, bounds: impl RangeBounds<usize>) -> LocalMemorySlice {
-        let (addr, length) = calc_addr_len(
-            bounds,
-            unsafe { *self.inner.mr }.addr as u64,
-            unsafe { *self.inner.mr }.length,
-        );
-        let sge = ffi::ibv_sge {
-            addr,
-            length: length.try_into().unwrap(),
-            lkey: unsafe { *self.inner.mr }.lkey,
-        };
-        LocalMemorySlice { _sge: sge }
-    }
 }
 
 /// Local memory slice.
@@ -1660,6 +1645,7 @@ impl MemoryRegionSlice {
 
     /// Make a slice of this memory region.
     pub fn slice(&self, bounds: impl RangeBounds<usize>) -> LocalMemorySlice {
+        let bounds = (..unsafe { *self.inner.mr }.length);
         let (addr, len) = calc_addr_len(bounds, self.addr(), self.len());
         LocalMemorySlice {
             _sge: ffi::ibv_sge {
